204c805db1d23d9b4badfb61a90b86f90201a512
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-06-10 04:50:34 +0100

    fixed circle radius

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,13 +7,13 @@ const casesTypeColors = {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
     half_op: "rgba(204, 16, 52, 0.5)",
-    multiplier: 800,
+    multiplier: 500,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 700,
   },
   deaths: {
     hex: "#fb4443",
@@ -22,6 +22,8 @@ const casesTypeColors = {
     multiplier: 2000,
   },
 };
+
+const balanceFactor = 0.3;
 
 const Map = ({ center, zoom, countries, type, onClick }) => {
   
@@ -32,7 +34,7 @@ const Map = ({ center, zoom, countries, type, onClick }) => {
         fillOpacity={0.4}
         fillColor={casesTypeColors[type].hex}
         color={casesTypeColors[type].hex}
-        radius={Math.sqrt(country[type]) * casesTypeColors[type].multiplier}
+        radius={Math.sqrt(country[type]) * casesTypeColors[type].multiplier * balanceFactor}
         key={`map-key-${id}`}
         value={country.countryInfo.iso3}
         onClick={(e) => onClick(e.target.options.value)}
